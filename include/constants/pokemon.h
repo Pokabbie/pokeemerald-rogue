8af1014f3d9b563f577d2d19c7614abd42b14df0
Abbie Potter <abbie@digitalqr.co.uk>
2022-07-11 22:00:35 +0100

    Change intro pokemon to Mareep

--- include/constants/pokemon.h
+++ include/constants/pokemon.h
@@ -91,7 +91,9 @@
 #define MAX_STAT_STAGE    12
 
 // Shiny odds
-#define SHINY_ODDS 8 // Actual probability is SHINY_ODDS/65536
+// RogueNote: odds
+//#define SHINY_ODDS 8 // Actual probability is SHINY_ODDS/65536
+#define SHINY_ODDS 1 // Actual probability is SHINY_ODDS/65536
 
 // Flags for Get(Box)MonData / Set(Box)MonData
 #define MON_DATA_PERSONALITY        0
