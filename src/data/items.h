52c39e34d9cf65e7b80c133976658c37a16eadf7
Abbie Potter <abbie@digitalqr.co.uk>
2022-07-17 22:40:15 +0100

    Added item shop that scales with difficulty

--- src/data/items.h
+++ src/data/items.h
@@ -17,7 +17,7 @@ const struct Item gItems[] =
     {
         .name = _("MASTER BALL"),
         .itemId = ITEM_MASTER_BALL,
-        .price = 0,
+        .price = 50000,
         .description = sMasterBallDesc,
         .pocket = POCKET_POKE_BALLS,
         .type = ITEM_MASTER_BALL - FIRST_BALL,
