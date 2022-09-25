d7f10de032d7e2e39a7e9a4ba01608cb6b8a365a
Abbie Potter <abbie@digitalqr.co.uk>
2022-09-25 17:32:45 +0100

    Setup bakery and moved berry patches

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1157,3 +1157,5 @@ Common_EventScript_LegendaryFlewAway::
 	.include "data/maps/Rogue_Hub_AdventureEnterance/scripts.inc"
 
 	.include "data/maps/Rogue_Hub_Debug/scripts.inc"
+
+	.include "data/maps/Rogue_Hub_Bakery/scripts.inc"
