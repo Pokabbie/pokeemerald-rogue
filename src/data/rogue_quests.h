de298eedf062930327432cdbb94ca1b9c915d995
Abbie Potter <abbie@digitalqr.co.uk>
2022-09-27 19:13:50 +0100

    Remove lvl up box and gym give HMs during run

--- src/data/rogue_quests.h
+++ src/data/rogue_quests.h
@@ -410,7 +410,12 @@ const struct RogueQuestConstants gRogueQuests[QUEST_CAPACITY + 1] =
                     "Trade whenever possible."
                 ),
         .rewards = { 
-            { .type=QUEST_REWARD_GIVE_MONEY, .params={ 20000 } }
+#ifdef ROGUE_EXPANSION
+            { .type=QUEST_REWARD_GIVE_ITEM, .params={ ITEM_ABILITY_CAPSULE, 1 } },
+            { .type=QUEST_REWARD_GIVE_ITEM, .params={ ITEM_ABILITY_PATCH, 1 } },
+#else
+            { .type=QUEST_REWARD_GIVE_MONEY, .params={ 20000 } },
+#endif
         }
     },
 
@@ -421,13 +426,8 @@ const struct RogueQuestConstants gRogueQuests[QUEST_CAPACITY + 1] =
                     "Have a POKéMON faint whilst\n"
                     "fighting a wild Wobbuffet."
                 ),
-        .rewards = { 
-#ifdef ROGUE_EXPANSION
-            { .type=QUEST_REWARD_GIVE_ITEM, .params={ ITEM_ABILITY_CAPSULE, 1 } },
-            { .type=QUEST_REWARD_GIVE_ITEM, .params={ ITEM_ABILITY_PATCH, 1 } },
-#else
-            { .type=QUEST_REWARD_GIVE_MONEY, .params={ 20000 } },
-#endif
+        .rewards = {
+            { .type=QUEST_REWARD_GIVE_MONEY, .params={ 3000 } },
         }
     },
 
