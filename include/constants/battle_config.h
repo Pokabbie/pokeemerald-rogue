3d9800776ac1eecd9afa6771ed1f51e6b67ac11a
Abbie Potter <abbie@digitalqr.co.uk>
2022-10-29 20:39:01 +0100

    Remove exp from captures

--- include/constants/battle_config.h
+++ include/constants/battle_config.h
@@ -142,7 +142,7 @@
 #define B_WHITEOUT_MONEY            GEN_3 // In Gen4+, the amount of money lost by losing a battle is determined by the amount of badges earned. Previously, it would cut the current money by half. (While this change was also in FRLG, for the sake of simplicity, setting this to GEN_3 will result in RSE behavior.)
 
 // Exp and stat settings
-#define B_EXP_CATCH                 GEN_7 // In Gen6+, Pokémon get experience from catching.
+#define B_EXP_CATCH                 GEN_3 // In Gen6+, Pokémon get experience from catching.
 #define B_TRAINER_EXP_MULTIPLIER    GEN_7 // In Gen7+, trainer battles no longer give a 1.5 multiplier to EXP gain.
 #define B_SPLIT_EXP                 GEN_7 // In Gen6+, all participating mon get full experience.
 #define B_SCALED_EXP                GEN_7 // In Gen5 and Gen7+, experience is weighted by level difference.
