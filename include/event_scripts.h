5a3c7678bdaf1671fd9cfc1558e8ce018b632617
Abbie Potter <abbie@digitalqr.co.uk>
2022-11-03 23:23:50 +0000

    ROM Cleanup - Removed all routes

--- include/event_scripts.h
+++ include/event_scripts.h
@@ -545,7 +545,6 @@ extern const u8 EventScript_FieldPoison[];
 extern const u8 EventScript_EggHatch[];
 extern const u8 AbnormalWeather_EventScript_EndEventAndCleanup_1[];
 extern const u8 IslandCave_EventScript_OpenRegiEntrance[];
-extern const u8 Route119_EventScript_ScottWonAtFortreeGymCall[];
 extern const u8 LittlerootTown_ProfessorBirchsLab_EventScript_ScottAboardSSTidalCall[];
 extern const u8 RustboroCity_Gym_EventScript_RegisterRoxanne[];
 extern const u8 MossdeepCity_SpaceCenter_2F_EventScript_RivalRayquazaCall[];
