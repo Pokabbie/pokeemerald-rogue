8d9434d6c64813d456015d5dd721237755f84022
Abbie Potter <abbie@digitalqr.co.uk>
2022-11-08 18:37:38 +0000

    EX Crit charm fix

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -1906,15 +1906,15 @@ s32 CalcCritChanceStage(u8 battlerAtk, u8 battlerDef, u32 move, bool32 recordAbi
                     + 2 * (holdEffectAtk == HOLD_EFFECT_LUCKY_PUNCH && gBattleMons[gBattlerAttacker].species == SPECIES_CHANSEY)
                     + 2 * BENEFITS_FROM_LEEK(battlerAtk, holdEffectAtk)
                     + (abilityAtk == ABILITY_SUPER_LUCK);
+
+        if(GetBattlerSide(gBattlerAttacker) == B_SIDE_OPPONENT)
+            critChance += GetCurseValue(EFFECT_CRIT_CHANCE);
+        else
+            critChance += GetCharmValue(EFFECT_CRIT_CHANCE);
+
+        if (critChance >= ARRAY_COUNT(sCriticalHitChance))
+            critChance = ARRAY_COUNT(sCriticalHitChance) - 1;
     }
-
-    if(GetBattlerSide(gBattlerAttacker) == B_SIDE_OPPONENT)
-        critChance += GetCurseValue(EFFECT_CRIT_CHANCE);
-    else
-        critChance += GetCharmValue(EFFECT_CRIT_CHANCE);
-
-    if (critChance >= ARRAY_COUNT(sCriticalHitChance))
-        critChance = ARRAY_COUNT(sCriticalHitChance) - 1;
 
     return critChance;
 }
