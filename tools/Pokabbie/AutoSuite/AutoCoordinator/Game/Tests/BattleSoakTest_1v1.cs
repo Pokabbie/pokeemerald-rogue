bb3fdceda37e3497a207a2a8aa545aaac9c16689
Abbie Potter <abbie@digitalqr.co.uk>
2022-11-27 18:42:05 +0000

    Missed files

---
+++ tools/Pokabbie/AutoSuite/AutoCoordinator/Game/Tests/BattleSoakTest_1v1.cs
@@ -0,0 +1,41 @@
+﻿using AutoCoordinator.GameConsole;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace AutoCoordinator.Game.Tests
+{
+	public class BattleSoakTest_1v1 : PokemonBattleTest
+	{
+		public BattleSoakTest_1v1() : base("1v1 Soak Test")
+		{
+		}
+
+		protected override void ConfigureInitialRun(PokemonGame game)
+		{
+		}
+
+		protected override void ConfigureTest(PokemonGame game)
+		{
+			game.ClearPlayerParty();
+			game.ClearEnemyParty();
+
+			int difficultyLevel = RNG.Next(0, 13);
+			LogTestMessage($"Difficulty level set to {difficultyLevel}");
+			game.SetRunDifficulty(difficultyLevel);
+
+			int weatherNum = RNG.Next(0, 17);
+			LogTestMessage($"Weather set to {weatherNum}");
+			game.SetWeather(weatherNum);
+
+			// Use Roxanne's ID so we get boss style generation
+			int teamSize = 1;
+			game.GeneratePlayerParty(265, teamSize);
+			game.GenerateEnemyParty(265, teamSize);
+
+			LogPlayerPartyInfo(game, teamSize);
+			LogEnemyPartyInfo(game, teamSize);
+		}
+	}
+}
