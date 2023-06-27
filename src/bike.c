ec6b6d946013399a2207ad29dd0336ac4512e0d7
Abbie Potter <abbie@digitalqr.co.uk>
2023-06-27 22:36:36 +0100

    Remove bike music

--- src/bike.c
+++ src/bike.c
@@ -982,8 +982,8 @@ void GetOnOffBike(u8 transitionFlags)
     else
     {
         SetPlayerAvatarTransitionFlags(transitionFlags);
-        Overworld_SetSavedMusic(MUS_CYCLING);
-        Overworld_ChangeMusicTo(MUS_CYCLING);
+        //Overworld_SetSavedMusic(MUS_CYCLING);
+        //Overworld_ChangeMusicTo(MUS_CYCLING);
     }
 }
 
