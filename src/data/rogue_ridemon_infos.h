5a50c8123b2d96e03600f2d8bffb5173c3d1e48b
Abbie Potter <abbie@digitalqr.co.uk>
2023-07-09 20:23:55 +0100

    Setup gen 1 ride stats

--- src/data/rogue_ridemon_infos.h
+++ src/data/rogue_ridemon_infos.h
@@ -98,15 +98,334 @@ static const struct RideMonInfo sRideMonInfo[NUM_SPECIES] =
             [RIDE_SPRITE_DIR_SIDE]  = { -5, -12, 3, 1, RIDER_SHOW_BEHIND },
         }
     },
-
+    [SPECIES_ARBOK] = 
+    {
+        .flags = RIDE_MON_FLAG_CAN_RIDE | RIDE_MON_FLAG_CAN_CLIMB,
+        .spriteInfo = 
+        {
+            [RIDE_SPRITE_DIR_UP]    = { 0, -6, 0, 1, RIDER_SHOW_INFRONT },
+            [RIDE_SPRITE_DIR_DOWN]  = { 0, -6, 0, 1, RIDER_SHOW_BEHIND },
+            [RIDE_SPRITE_DIR_SIDE]  = { -3, -6, 2, 1, RIDER_SHOW_BEHIND },
+        }
+    },
+    [SPECIES_NIDOQUEEN] = 
+    {
+        .flags = RIDE_MON_FLAG_CAN_RIDE | RIDE_MON_FLAG_CAN_CLIMB,
+        .spriteInfo = 
+        {
+            [RIDE_SPRITE_DIR_UP]    = { 0, -6, 0, 1, RIDER_SHOW_INFRONT },
+            [RIDE_SPRITE_DIR_DOWN]  = { 0, -6, 0, 1, RIDER_SHOW_BEHIND },
+            [RIDE_SPRITE_DIR_SIDE]  = { -5, -6, 2, 1, RIDER_SHOW_INFRONT },
+        }
+    },
+    [SPECIES_NIDOKING] = 
+    {
+        .flags = RIDE_MON_FLAG_CAN_RIDE | RIDE_MON_FLAG_CAN_CLIMB,
+        .spriteInfo = 
+        {
+            [RIDE_SPRITE_DIR_UP]    = { 0, -6, 0, 1, RIDER_SHOW_INFRONT },
+            [RIDE_SPRITE_DIR_DOWN]  = { 0, -6, 0, 1, RIDER_SHOW_BEHIND },
+            [RIDE_SPRITE_DIR_SIDE]  = { -5, -6, 2, 1, RIDER_SHOW_INFRONT },
+        }
+    },
+    [SPECIES_PARASECT] = 
+    {
+        .flags = RIDE_MON_FLAG_CAN_RIDE,
+        .spriteInfo = 
+        {
+            [RIDE_SPRITE_DIR_UP]    = { 0, -6, 0, 1, RIDER_SHOW_INFRONT },
+            [RIDE_SPRITE_DIR_DOWN]  = { 0, -6, 0, 1, RIDER_SHOW_BEHIND },
+            [RIDE_SPRITE_DIR_SIDE]  = { -5, -6, 2, 1, RIDER_SHOW_INFRONT },
+        }
+    },
+    [SPECIES_PRIMEAPE] = 
+    {
+        .flags = RIDE_MON_FLAG_CAN_RIDE | RIDE_MON_FLAG_CAN_CLIMB,
+        .spriteInfo = 
+        {
+            [RIDE_SPRITE_DIR_UP]    = { 0, -6, 0, 1, RIDER_SHOW_INFRONT },
+            [RIDE_SPRITE_DIR_DOWN]  = { 0, -6, 0, 1, RIDER_SHOW_BEHIND },
+            [RIDE_SPRITE_DIR_SIDE]  = { -5, -6, 2, 1, RIDER_SHOW_INFRONT },
+        }
+    },
+    [SPECIES_ARCANINE] = 
+    {
+        .flags = RIDE_MON_FLAG_CAN_RIDE,
+        .spriteInfo = 
+        {
+            [RIDE_SPRITE_DIR_UP]    = { 0, -8, 0, 1, RIDER_SHOW_INFRONT },
+            [RIDE_SPRITE_DIR_DOWN]  = { 0, -2, 0, 1, RIDER_SHOW_BEHIND },
+            [RIDE_SPRITE_DIR_SIDE]  = { -2, -8, 2, 1, RIDER_SHOW_INFRONT },
+        }
+    },
+    [SPECIES_POLIWRATH] = 
+    {
+        .flags = RIDE_MON_FLAG_CAN_RIDE | RIDE_MON_FLAG_CAN_SWIM,
+        .spriteInfo = 
+        {
+            [RIDE_SPRITE_DIR_UP]    = { 0, -8, 0, 1, RIDER_SHOW_INFRONT },
+            [RIDE_SPRITE_DIR_DOWN]  = { 0, -10, 0, 1, RIDER_SHOW_BEHIND },
+            [RIDE_SPRITE_DIR_SIDE]  = { -4, -8, 2, 1, RIDER_SHOW_INFRONT },
+        }
+    },
+    [SPECIES_ALAKAZAM] = 
+    {
+        .flags = RIDE_MON_FLAG_CAN_RIDE | RIDE_MON_FLAG_CAN_FLY,
+        .spriteInfo = 
+        {
+            [RIDE_SPRITE_DIR_UP]    = { 0, -8, 0, 1, RIDER_SHOW_INFRONT },
+            [RIDE_SPRITE_DIR_DOWN]  = { 0, -10, 0, 1, RIDER_SHOW_BEHIND },
+            [RIDE_SPRITE_DIR_SIDE]  = { -4, -8, 2, 1, RIDER_SHOW_BEHIND },
+        }
+    },
+    [SPECIES_MACHAMP] = 
+    {
+        .flags = RIDE_MON_FLAG_CAN_RIDE | RIDE_MON_FLAG_CAN_CLIMB,
+        .spriteInfo = 
+        {
+            [RIDE_SPRITE_DIR_UP]    = { 0, -8, 0, 1, RIDER_SHOW_INFRONT },
+            [RIDE_SPRITE_DIR_DOWN]  = { 0, -10, 0, 1, RIDER_SHOW_BEHIND },
+            [RIDE_SPRITE_DIR_SIDE]  = { -4, -8, 2, 1, RIDER_SHOW_BEHIND },
+        }
+    },
+    [SPECIES_VICTREEBEL] = 
+    {
+        .flags = RIDE_MON_FLAG_CAN_RIDE,
+        .spriteInfo = 
+        {
+            [RIDE_SPRITE_DIR_UP]    = { 0, -8, 0, 1, RIDER_SHOW_INFRONT },
+            [RIDE_SPRITE_DIR_DOWN]  = { 0, -10, 0, 1, RIDER_SHOW_BEHIND },
+            [RIDE_SPRITE_DIR_SIDE]  = { -4, -8, 2, 1, RIDER_SHOW_BEHIND },
+        }
+    },
+    [SPECIES_TENTACRUEL] = 
+    {
+        .flags = RIDE_MON_FLAG_CAN_RIDE | RIDE_MON_FLAG_CAN_SWIM,
+        .spriteInfo = 
+        {
+            [RIDE_SPRITE_DIR_UP]    = { 0, -8, 0, 1, RIDER_SHOW_INFRONT },
+            [RIDE_SPRITE_DIR_DOWN]  = { 0, -10, 0, 1, RIDER_SHOW_BEHIND },
+            [RIDE_SPRITE_DIR_SIDE]  = { -4, -8, 2, 1, RIDER_SHOW_INFRONT },
+        }
+    },
+    [SPECIES_GOLEM] = 
+    {
+        .flags = RIDE_MON_FLAG_CAN_RIDE | RIDE_MON_FLAG_CAN_CLIMB,
+        .spriteInfo = 
+        {
+            [RIDE_SPRITE_DIR_UP]    = { 0, -8, 0, 1, RIDER_SHOW_INFRONT },
+            [RIDE_SPRITE_DIR_DOWN]  = { 0, -10, 0, 1, RIDER_SHOW_BEHIND },
+            [RIDE_SPRITE_DIR_SIDE]  = { -4, -8, 2, 1, RIDER_SHOW_INFRONT },
+        }
+    },
+    [SPECIES_RAPIDASH] = 
+    {
+        .flags = RIDE_MON_FLAG_CAN_RIDE,
+        .spriteInfo = 
+        {
+            [RIDE_SPRITE_DIR_UP]    = { 0, -8, 0, 1, RIDER_SHOW_INFRONT },
+            [RIDE_SPRITE_DIR_DOWN]  = { 0, -10, 0, 1, RIDER_SHOW_BEHIND },
+            [RIDE_SPRITE_DIR_SIDE]  = { -4, -8, 2, 1, RIDER_SHOW_INFRONT },
+        }
+    },
+    [SPECIES_SLOWBRO] = 
+    {
+        .flags = RIDE_MON_FLAG_CAN_RIDE | RIDE_MON_FLAG_CAN_SWIM,
+        .spriteInfo = 
+        {
+            [RIDE_SPRITE_DIR_UP]    = { 0, -8, 0, 1, RIDER_SHOW_INFRONT },
+            [RIDE_SPRITE_DIR_DOWN]  = { 0, -10, 0, 1, RIDER_SHOW_BEHIND },
+            [RIDE_SPRITE_DIR_SIDE]  = { -4, -8, 2, 1, RIDER_SHOW_INFRONT },
+        }
+    },
+    [SPECIES_DODRIO] = 
+    {
+        .flags = RIDE_MON_FLAG_CAN_RIDE,
+        .spriteInfo = 
+        {
+            [RIDE_SPRITE_DIR_UP]    = { 0, -4, 0, 1, RIDER_SHOW_INFRONT },
+            [RIDE_SPRITE_DIR_DOWN]  = { 0, -6, 0, 1, RIDER_SHOW_BEHIND },
+            [RIDE_SPRITE_DIR_SIDE]  = { -5, -6, 0, 1, RIDER_SHOW_INFRONT },
+        }
+    },
+    [SPECIES_DEWGONG] = 
+    {
+        .flags = RIDE_MON_FLAG_CAN_RIDE | RIDE_MON_FLAG_CAN_SWIM,
+        .spriteInfo = 
+        {
+            [RIDE_SPRITE_DIR_UP]    = { 0, -4, 0, 1, RIDER_SHOW_INFRONT },
+            [RIDE_SPRITE_DIR_DOWN]  = { 0, -6, 0, 1, RIDER_SHOW_BEHIND },
+            [RIDE_SPRITE_DIR_SIDE]  = { -5, -6, 0, 1, RIDER_SHOW_INFRONT },
+        }
+    },
+    [SPECIES_MUK] = 
+    {
+        .flags = RIDE_MON_FLAG_CAN_RIDE,
+        .spriteInfo = 
+        {
+            [RIDE_SPRITE_DIR_UP]    = { 0, -4, 0, 1, RIDER_SHOW_INFRONT },
+            [RIDE_SPRITE_DIR_DOWN]  = { 0, -6, 0, 1, RIDER_SHOW_BEHIND },
+            [RIDE_SPRITE_DIR_SIDE]  = { -5, -6, 0, 1, RIDER_SHOW_INFRONT },
+        }
+    },
+    [SPECIES_ONIX] = 
+    {
+        .flags = RIDE_MON_FLAG_CAN_RIDE | RIDE_MON_FLAG_CAN_CLIMB,
+        .spriteInfo = 
+        {
+            [RIDE_SPRITE_DIR_UP]    = { 0, -6, 0, 1, RIDER_SHOW_INFRONT },
+            [RIDE_SPRITE_DIR_DOWN]  = { 0, -10, 0, 1, RIDER_SHOW_BEHIND },
+            [RIDE_SPRITE_DIR_SIDE]  = { -10, -8, 2, 1, RIDER_SHOW_INFRONT },
+        }
+    },
+    [SPECIES_ELECTRODE] = 
+    {
+        .flags = RIDE_MON_FLAG_CAN_RIDE,
+        .spriteInfo = 
+        {
+            [RIDE_SPRITE_DIR_UP]    = { 0, -6, 0, 1, RIDER_SHOW_INFRONT },
+            [RIDE_SPRITE_DIR_DOWN]  = { 0, -10, 0, 1, RIDER_SHOW_BEHIND },
+            [RIDE_SPRITE_DIR_SIDE]  = { -10, -8, 2, 1, RIDER_SHOW_INFRONT },
+        }
+    },
+    [SPECIES_WEEZING] = 
+    {
+        .flags = RIDE_MON_FLAG_CAN_RIDE | RIDE_MON_FLAG_CAN_FLY,
+        .spriteInfo = 
+        {
+            [RIDE_SPRITE_DIR_UP]    = { 0, -6, 0, 1, RIDER_SHOW_INFRONT },
+            [RIDE_SPRITE_DIR_DOWN]  = { 0, -10, 0, 1, RIDER_SHOW_BEHIND },
+            [RIDE_SPRITE_DIR_SIDE]  = { -10, -8, 2, 1, RIDER_SHOW_INFRONT },
+        }
+    },
+    [SPECIES_RHYDON] = 
+    {
+        .flags = RIDE_MON_FLAG_CAN_RIDE | RIDE_MON_FLAG_CAN_CLIMB,
+        .spriteInfo = 
+        {
+            [RIDE_SPRITE_DIR_UP]    = { 0, -6, 0, 1, RIDER_SHOW_INFRONT },
+            [RIDE_SPRITE_DIR_DOWN]  = { 0, -10, 0, 1, RIDER_SHOW_BEHIND },
+            [RIDE_SPRITE_DIR_SIDE]  = { -4, -8, 2, 1, RIDER_SHOW_INFRONT },
+        }
+    },
+    [SPECIES_KANGASKHAN] = 
+    {
+        .flags = RIDE_MON_FLAG_CAN_RIDE | RIDE_MON_FLAG_CAN_CLIMB,
+        .spriteInfo = 
+        {
+            [RIDE_SPRITE_DIR_UP]    = { 0, -6, 0, 1, RIDER_SHOW_INFRONT },
+            [RIDE_SPRITE_DIR_DOWN]  = { 0, -10, 0, 1, RIDER_SHOW_BEHIND },
+            [RIDE_SPRITE_DIR_SIDE]  = { -4, -8, 2, 1, RIDER_SHOW_INFRONT },
+        }
+    },
+    [SPECIES_SEAKING] = 
+    {
+        .flags = RIDE_MON_FLAG_CAN_RIDE | RIDE_MON_FLAG_CAN_SWIM,
+        .spriteInfo = 
+        {
+            [RIDE_SPRITE_DIR_UP]    = { 0, -6, 0, 1, RIDER_SHOW_INFRONT },
+            [RIDE_SPRITE_DIR_DOWN]  = { 0, -10, 0, 1, RIDER_SHOW_BEHIND },
+            [RIDE_SPRITE_DIR_SIDE]  = { -4, -8, 2, 1, RIDER_SHOW_INFRONT },
+        }
+    },
+    [SPECIES_STARMIE] = 
+    {
+        .flags = RIDE_MON_FLAG_CAN_RIDE | RIDE_MON_FLAG_CAN_SWIM,
+        .spriteInfo = 
+        {
+            [RIDE_SPRITE_DIR_UP]    = { 0, -6, 0, 1, RIDER_SHOW_INFRONT },
+            [RIDE_SPRITE_DIR_DOWN]  = { 0, -10, 0, 1, RIDER_SHOW_BEHIND },
+            [RIDE_SPRITE_DIR_SIDE]  = { -4, -8, 2, 1, RIDER_SHOW_INFRONT },
+        }
+    },
+    [SPECIES_TAUROS] = 
+    {
+        .flags = RIDE_MON_FLAG_CAN_RIDE,
+        .spriteInfo = 
+        {
+            [RIDE_SPRITE_DIR_UP]    = { 0, -8, 0, 3, RIDER_SHOW_INFRONT },
+            [RIDE_SPRITE_DIR_DOWN]  = { 0, -9, 0, 3, RIDER_SHOW_INFRONT },
+            [RIDE_SPRITE_DIR_SIDE]  = { 0, -8, 2, 1, RIDER_SHOW_INFRONT },
+        }
+    },
+    [SPECIES_GYARADOS] = 
+    {
+        .flags = RIDE_MON_FLAG_CAN_RIDE | RIDE_MON_FLAG_CAN_SWIM,
+        .spriteInfo = 
+        {
+            [RIDE_SPRITE_DIR_UP]    = { 0, -8, 0, 3, RIDER_SHOW_INFRONT },
+            [RIDE_SPRITE_DIR_DOWN]  = { 0, -12, 0, 3, RIDER_SHOW_BEHIND },
+            [RIDE_SPRITE_DIR_SIDE]  = { -3, -10, 4, 1, RIDER_SHOW_INFRONT },
+        }
+    },
     [SPECIES_LAPRAS] = 
     {
-        .flags = RIDE_MON_FLAG_CAN_RIDE | RIDE_MON_FLAG_CAN_SWIM | RIDE_MON_FLAG_CAN_FLY,
+        .flags = RIDE_MON_FLAG_CAN_RIDE | RIDE_MON_FLAG_CAN_SWIM,
         .spriteInfo = 
         {
             [RIDE_SPRITE_DIR_UP]    = { 0, -4, 0, 2, RIDER_SHOW_INFRONT },
             [RIDE_SPRITE_DIR_DOWN]  = { 0, -6, 0, 2, RIDER_SHOW_BEHIND },
             [RIDE_SPRITE_DIR_SIDE]  = { -3, -4, 6, 2, RIDER_SHOW_INFRONT },
         }
-    }
+    },
+    [SPECIES_AERODACTYL] = 
+    {
+        .flags = RIDE_MON_FLAG_CAN_RIDE | RIDE_MON_FLAG_CAN_FLY,
+        .spriteInfo = 
+        {
+            [RIDE_SPRITE_DIR_UP]    = { 0, -6, 0, 1, RIDER_SHOW_INFRONT },
+            [RIDE_SPRITE_DIR_DOWN]  = { 0, -8, 0, 1, RIDER_SHOW_BEHIND },
+            [RIDE_SPRITE_DIR_SIDE]  = { -5, -10, 3, 1, RIDER_SHOW_BEHIND },
+        }
+    },
+    [SPECIES_SNORLAX] = 
+    {
+        .flags = RIDE_MON_FLAG_CAN_RIDE | RIDE_MON_FLAG_CAN_CLIMB,
+        .spriteInfo = 
+        {
+            [RIDE_SPRITE_DIR_UP]    = { 0, -6, 0, 1, RIDER_SHOW_INFRONT },
+            [RIDE_SPRITE_DIR_DOWN]  = { 0, -8, 0, 1, RIDER_SHOW_BEHIND },
+            [RIDE_SPRITE_DIR_SIDE]  = { -8, -10, 3, 1, RIDER_SHOW_INFRONT },
+        }
+    },
+    [SPECIES_ARTICUNO] = 
+    {
+        .flags = RIDE_MON_FLAG_CAN_RIDE | RIDE_MON_FLAG_CAN_FLY,
+        .spriteInfo = 
+        {
+            [RIDE_SPRITE_DIR_UP]    = { 0, -6, 0, 1, RIDER_SHOW_INFRONT },
+            [RIDE_SPRITE_DIR_DOWN]  = { 0, -10, 0, 1, RIDER_SHOW_BEHIND },
+            [RIDE_SPRITE_DIR_SIDE]  = { -4, -10, 2, 1, RIDER_SHOW_BEHIND },
+        }
+    },
+    [SPECIES_MOLTRES] = 
+    {
+        .flags = RIDE_MON_FLAG_CAN_RIDE | RIDE_MON_FLAG_CAN_FLY,
+        .spriteInfo = 
+        {
+            [RIDE_SPRITE_DIR_UP]    = { 0, -6, 0, 1, RIDER_SHOW_INFRONT },
+            [RIDE_SPRITE_DIR_DOWN]  = { 0, -10, 0, 1, RIDER_SHOW_BEHIND },
+            [RIDE_SPRITE_DIR_SIDE]  = { -6, -10, 2, 1, RIDER_SHOW_BEHIND },
+        }
+    },
+    [SPECIES_ZAPDOS] = 
+    {
+        .flags = RIDE_MON_FLAG_CAN_RIDE | RIDE_MON_FLAG_CAN_FLY,
+        .spriteInfo = 
+        {
+            [RIDE_SPRITE_DIR_UP]    = { 0, -8, 0, 1, RIDER_SHOW_INFRONT },
+            [RIDE_SPRITE_DIR_DOWN]  = { 0, -10, 0, 1, RIDER_SHOW_BEHIND },
+            [RIDE_SPRITE_DIR_SIDE]  = { -8, -10, 2, 1, RIDER_SHOW_BEHIND },
+        }
+    },
+    [SPECIES_DRAGONITE] = 
+    {
+        .flags = RIDE_MON_FLAG_CAN_RIDE | RIDE_MON_FLAG_CAN_FLY,
+        .spriteInfo = 
+        {
+            [RIDE_SPRITE_DIR_UP]    = { 0, -8, 0, 1, RIDER_SHOW_INFRONT },
+            [RIDE_SPRITE_DIR_DOWN]  = { 0, -10, 0, 1, RIDER_SHOW_BEHIND },
+            [RIDE_SPRITE_DIR_SIDE]  = { -6, -10, 2, 1, RIDER_SHOW_BEHIND },
+        }
+    },
 };
