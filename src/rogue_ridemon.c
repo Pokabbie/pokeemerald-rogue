ca90997b0b9c96c504ad92799d1c77846e528dfa
Abbie Potter <abbie@digitalqr.co.uk>
2023-07-10 17:47:04 +0100

    Re-exported gen 1 with new tool

--- src/rogue_ridemon.c
+++ src/rogue_ridemon.c
@@ -15,6 +15,51 @@
 #include "rogue_followmon.h"
 #include "rogue_ridemon.h"
 
+enum
+{
+    RIDE_SPRITE_DIR_UP,
+    RIDE_SPRITE_DIR_DOWN,
+    RIDE_SPRITE_DIR_SIDE,
+    RIDE_SPRITE_DIR_COUNT
+};
+
+enum
+{
+    RIDER_SHOW_BEHIND,
+    RIDER_SHOW_INFRONT,
+};
+
+enum
+{
+    RIDE_MOVEMENT_SLOW,
+    RIDE_MOVEMENT_ACCELERATE_AVERAGE,
+    RIDE_MOVEMENT_AVERAGE,
+    RIDE_MOVEMENT_ACCELERATE_FAST,
+    RIDE_MOVEMENT_FAST,
+};
+
+#define RIDE_MON_FLAG_NONE          (0)
+#define RIDE_MON_FLAG_CAN_RIDE      (1 << 0)
+#define RIDE_MON_FLAG_CAN_SWIM      (1 << 1)
+#define RIDE_MON_FLAG_CAN_CLIMB     (1 << 2)
+#define RIDE_MON_FLAG_CAN_FLY       (1 << 3)
+
+
+struct RideMonSpriteInfo
+{
+    s8 playerX;
+    s8 playerY;
+    s8 monX;
+    s8 monY;
+    u8 playerRendersInFront : 1;
+};
+
+struct RideMonInfo
+{
+    u8 flags;
+    struct RideMonSpriteInfo spriteInfo[RIDE_SPRITE_DIR_COUNT];
+};
+
 #include "data/rogue_ridemon_infos.h"
 
 struct TestData
