369be6e3c0b503eb2f10db9cc6037352fe1a6516
Abbie Potter <abbie@digitalqr.co.uk>
2023-07-23 17:25:05 +0100

    [BUILDFIX] Case correction

--- make_tools.mk
+++ make_tools.mk
@@ -2,7 +2,7 @@
 MAKEFLAGS += --no-print-directory
 
 # Inclusive list. If you don't want a tool to be built, don't add it here.
-TOOLDIRS := tools/aif2pcm tools/bin2c tools/gbafix tools/gbagfx tools/jsonproc tools/mapjson tools/mid2agb tools/preproc tools/ramscrgen tools/rsfont tools/scaninc tools/Pokabbie/Build/Memorystats
+TOOLDIRS := tools/aif2pcm tools/bin2c tools/gbafix tools/gbagfx tools/jsonproc tools/mapjson tools/mid2agb tools/preproc tools/ramscrgen tools/rsfont tools/scaninc tools/Pokabbie/Build/MemoryStats
 
 .PHONY: all $(TOOLDIRS)
 
