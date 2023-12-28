7d877db4f81039ee7fc26bb20e37df12dd4a4967
Abbie Potter <abbie@digitalqr.co.uk>
2023-12-28 16:32:30 +0000

    Fixed object event macro not aligning correctly

--- include/global.fieldmap.h
+++ include/global.fieldmap.h
@@ -74,12 +74,10 @@ struct ObjectEventTemplate
     /*0x09*/ u8 movementType;
     /*0x0A*/ u16 movementRangeX:4;
              u16 movementRangeY:4;
-             //u16 padding2:8;
     /*0x0C*/ u16 trainerType;
     /*0x0E*/ u16 trainerRange_berryTreeId;
     /*0x10*/ const u8 *script;
     /*0x14*/ u16 flagId;
-    /*0x16*/ //u8 padding3[2];
 };
 
 struct WarpEvent
