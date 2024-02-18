f660b3f2b98529141ead193755593731a2c56c2a
Abbie Potter <abbie@digitalqr.co.uk>
2024-02-18 16:04:07 +0000

    Setup separate build tasks for debug/release (Quick and hacky, should come back and clean this up at some point)

--- scripts/launch_build_debug.bat
+++ scripts/launch_build_debug.bat
@@ -6,7 +6,7 @@ cd %~dp0../
 @rem grep regex to filter out the clear command that's coming from somewhere and losing the output
 @rem call C:\devkitPro\msys2\msys2_shell.bat -mingw64 -here -defterm -no-start launch_build.sh | grep 's/.\[H.\[2J//'
 
-call C:\devkitPro\msys2\msys2_shell.bat -mingw64 -here -defterm -no-start %~dp0launch_build.sh
+call C:\devkitPro\msys2\msys2_shell.bat -mingw64 -here -defterm -no-start %~dp0launch_build_debug.sh
 
 if not !ERRORLEVEL!==0 (
    echo "Successful! (Output discarded)"
---
+++ scripts/launch_build_release.bat
@@ -0,0 +1,14 @@
+@echo off
+setlocal enabledelayedexpansion
+cd %~dp0../
+
+@rem == THIS ONLY WORKS FOR FAILURES?? ==
+@rem grep regex to filter out the clear command that's coming from somewhere and losing the output
+@rem call C:\devkitPro\msys2\msys2_shell.bat -mingw64 -here -defterm -no-start launch_build.sh | grep 's/.\[H.\[2J//'
+
+call C:\devkitPro\msys2\msys2_shell.bat -mingw64 -here -defterm -no-start %~dp0launch_build_release.sh
+
+if not !ERRORLEVEL!==0 (
+   echo "Successful! (Output discarded)"
+)
+exit /b !ERRORLEVEL!
