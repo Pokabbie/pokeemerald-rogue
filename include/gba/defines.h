648b085f653f87e5a3ea0cc8a9bb48ea61d61b38
Abbie Potter <abbie@digitalqr.co.uk>
2024-03-04 18:45:28 +0000

    Compile fix

--- include/gba/defines.h
+++ include/gba/defines.h
@@ -17,6 +17,7 @@
 #endif
 
 #define ALIGNED(n) __attribute__((aligned(n)))
+#define PACKED __attribute__((packed))
 
 #define SOUND_INFO_PTR (*(struct SoundInfo **)0x3007FF0)
 #define INTR_CHECK     (*(u16 *)0x3007FF8)
@@ -30,14 +31,12 @@
 #define IWRAM_START 0x03000000
 #define IWRAM_END   (IWRAM_START + 0x8000)
 
-#define PLTT      0x5000000
-#define PLTT_SIZE 0x400
-
-#define BG_PLTT      PLTT
-#define BG_PLTT_SIZE 0x200
-
-#define OBJ_PLTT      (PLTT + 0x200)
+#define PLTT          0x5000000
+#define BG_PLTT       PLTT
+#define BG_PLTT_SIZE  0x200
+#define OBJ_PLTT      (PLTT + BG_PLTT_SIZE)
 #define OBJ_PLTT_SIZE 0x200
+#define PLTT_SIZE     (BG_PLTT_SIZE + OBJ_PLTT_SIZE)
 
 #define VRAM      0x6000000
 #define VRAM_SIZE 0x18000
@@ -67,11 +66,23 @@
 
 #define ROM_HEADER_SIZE   0xC0
 
+// Dimensions of a tile in pixels
+#define TILE_WIDTH  8
+#define TILE_HEIGHT 8
+
+// Dimensions of the GBA screen in pixels
 #define DISPLAY_WIDTH  240
 #define DISPLAY_HEIGHT 160
 
-#define TILE_SIZE_4BPP 32
-#define TILE_SIZE_8BPP 64
+// Dimensions of the GBA screen in tiles
+#define DISPLAY_TILE_WIDTH  (DISPLAY_WIDTH / TILE_WIDTH)
+#define DISPLAY_TILE_HEIGHT (DISPLAY_HEIGHT / TILE_HEIGHT)
+
+// Size of different tile formats in bytes
+#define TILE_SIZE(bpp)((bpp) * TILE_WIDTH * TILE_HEIGHT / 8)
+#define TILE_SIZE_1BPP TILE_SIZE(1) // 8
+#define TILE_SIZE_4BPP TILE_SIZE(4) // 32
+#define TILE_SIZE_8BPP TILE_SIZE(8) // 64
 
 #define TILE_OFFSET_4BPP(n) ((n) * TILE_SIZE_4BPP)
 #define TILE_OFFSET_8BPP(n) ((n) * TILE_SIZE_8BPP)
