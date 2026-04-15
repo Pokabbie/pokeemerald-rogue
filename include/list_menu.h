039b0052774d4bff3a76bb4caaa5d00319a5107a
Abbie Potter <abbiepot97@gmail.com>
2026-04-15 20:44:46 +0100

    Merge fix

--- include/list_menu.h
+++ include/list_menu.h
@@ -27,13 +27,6 @@ enum {
     SCROLL_ARROW_DOWN
 };
 
-enum {
-    CURSOR_BLACK_ARROW,
-    CURSOR_INVISIBLE,
-    CURSOR_RED_OUTLINE,
-    CURSOR_RED_ARROW,
-};
-
 struct ListMenu;
 
 struct ListMenuItem
