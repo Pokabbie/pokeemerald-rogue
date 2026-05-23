99bb82039a174745636f2e0728b3062242241d1d
Abbie Potter <abbiepot97@gmail.com>
2026-05-23 12:06:27 +0100

    Weather won't kill enraged mons

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -7839,6 +7839,14 @@ static void Cmd_weatherdamage(void)
     if (gAbsentBattlerFlags & gBitTable[gBattlerAttacker])
         gBattleMoveDamage = 0;
 
+    if(ActiveAlphaMonEndure(gBattlerAttacker))
+    {
+        if(gBattleMons[gBattlerAttacker].hp != 0 && gBattleMoveDamage >= gBattleMons[gBattlerAttacker].hp)
+        {
+            gBattleMoveDamage = gBattleMons[gBattlerAttacker].hp - 1;
+        }
+    }
+
     gBattlescriptCurrInstr++;
 }
 
